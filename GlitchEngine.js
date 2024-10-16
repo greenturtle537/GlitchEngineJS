a9e429b2c1433f1064e3b12ef261db004f3b20da
greenturtle537 <support@glitchtech.top>
2024-10-16 00:14:35 -0400

    Update GlitchEngine.js

--- GlitchEngine.js
+++ GlitchEngine.js
@@ -1,7 +1,7 @@
 //This file exists for semantics
-import KeyBehaviour from "./src/keyBehaviour/keyBehaviour.js";
-import CanvasWindow from "./src/objectWindows/CanvasWindow.js";
-import GraphicWindow from "./src/objectWindows/GraphicWindow.js";
-import TextWindow from "./src/objectWindows/TextWindow.js";
+import {KeyBehaviour} from "./src/keyBehaviour/keyBehaviour.js";
+import {CanvasWindow} from "./src/objectWindows/CanvasWindow.js";
+import {GraphicWindow} from "./src/objectWindows/GraphicWindow.js";
+import {TextWindow} from "./src/objectWindows/TextWindow.js";
 
 export { CanvasWindow, GraphicWindow, TextWindow, KeyBehaviour };
